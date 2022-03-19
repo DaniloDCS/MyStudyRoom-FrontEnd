30c3eb12ecac223039ac4011b2495b838ec35357
Danilo <danillo.ifpb@gmail.com>
2022-03-19 01:03:08 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import 'dotenv/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
   plugins: [react()]
 })
